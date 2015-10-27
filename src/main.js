b43fd62cbca37034dc20c45780b0ce9bbc371d4d
Nicole Chung <nchung@nascentdigital.com>
2015-10-27 12:04:32 -0400

    adding sass compilation - goes to a separate stylesheet, not embedded styles

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+require('../sass/style.scss');
+
 import React from 'react';
 
 class Hello extends React.Component {
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var webpack = require('webpack');
 
 module.exports = {
@@ -6,7 +7,17 @@ module.exports = {
   output: { path: __dirname, filename: 'bundle.js' },
   module: {
     loaders: [
-      { test: /.jsx?$/, loader: 'babel-loader', exclude: /node_modules/ }
+      { test: /.jsx?$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.css/, loader: ExtractTextPlugin.extract('css') },
+      { test: /\.scss$/, loader: ExtractTextPlugin.extract('!css!sass?sourceMap') }
     ]
   },
+  sassLoader: {
+    includePaths: [ path.resolve(__dirname, './sass')]
+  },
+  plugins: [
+    new ExtractTextPlugin('css/app.css',{
+            allChunks: true
+        })
+  ]
 };
